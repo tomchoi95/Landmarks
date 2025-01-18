b530fd3c3b9afcfbe44becb47006521d2de35366
Tom Bum Su Choi <qjatn0545@naver.com>
2025-01-18 13:00:52 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 최범수 on 2025-01-18.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
