23d3a17de85b58d514e9c2a61aa20f34074ce22d
Tom Bum Su Choi <qjatn0545@naver.com>
2025-01-18 14:32:36 +0900

    section 5

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,25 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by 최범수 on 2025-01-18.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+            span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        )
+    }
+}
+
+#Preview {
+    MapView()
+}
