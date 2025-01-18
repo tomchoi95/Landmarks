30720c2003daefe54c0c0a4e9031df25438379d9
Tom Bum Su Choi <qjatn0545@naver.com>
2025-01-18 19:35:35 +0900

    네비게이션 세팅

---
+++ Landmarks/View/LandmarkDetail.swift
@@ -0,0 +1,51 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by 최범수 on 2025-01-18.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                
+                HStack {
+                    Text("Joshua National Tree Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+            
+            Spacer()
+        }    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
