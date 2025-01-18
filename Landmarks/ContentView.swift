089af3b6bba47131f3d28a022987884720703a22
Tom Bum Su Choi <qjatn0545@naver.com>
2025-01-18 14:55:26 +0900

    끝

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,20 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-                .multilineTextAlignment(.center)
+        VStack {
+            
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
                 
-            HStack {
-                Text("Joshua National Tree Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+                HStack {
+                    Text("Joshua National Tree Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                
+                Text("Descriptive text goes here.")
             }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
--- Landmarks/MapView.swift
+++ Landmarks/MapView.swift
@@ -9,15 +9,18 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    var body: some View {
-        Map(initialPosition: .region(region))
-    }
+    
     private var region: MKCoordinateRegion {
         MKCoordinateRegion(
             center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
             span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
         )
     }
+    
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+    
 }
 
 #Preview {
